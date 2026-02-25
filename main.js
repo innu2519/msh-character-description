126eddc2f15db388c3b1ca0ea732c024bc43a678
innu2519 <longzhijiebanben432@gmail.com>
2026-02-26 01:50:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const charaData = {
     birthday: "2月27日",
     blood: "O型",
     height: "151cm",
-    work:`電柱`,
+    work:`<a href="https://minecraft-mcworld.com/131508/">電柱</a>`,
     mark:`⛓️🥀`,
     description: 
 `電柱の中で『ゴミ』を管理する少女。のじゃロリ。
@@ -27,7 +27,7 @@ const charaData = {
   birthday: "7月2日",
   blood: "A型",
   height: "172cm",
-  work: "電柱",
+  work: `<a href="https://minecraft-mcworld.com/131508/">電柱</a>`,
   mark: "🚬💣",
   description:
 `爆弾少年。
@@ -44,7 +44,7 @@ const charaData = {
   birthday: "11月15日",
   blood: "A型",
   height: "160cm",
-  work: "電柱",
+  work: `<a href="https://minecraft-mcworld.com/131508/">電柱</a>`,
   mark: "🎤",
   description:
 `声帯の無い少女。
@@ -64,7 +64,7 @@ Tale『うたえ』参照。`
   birthday: "5月22日",
   blood: "O型",
   height: "165cm",
-  work: "勿忘草",
+  work: `<a href="https://minecraft-mcworld.com/94746/">Re:勿忘草</a>`,
   mark: "💚🧱",
   description:
 `裏世界にいるお姉さん。
@@ -86,7 +86,7 @@ Tale『うたえ』参照。`
   birthday: "11月30日",
   blood: "O型",
   height: "180cm",
-  work: "勿忘草",
+  work: `<a href="https://minecraft-mcworld.com/94746/">Re:勿忘草</a>`,
   mark: "❓",
   description:
 `うるさい人。
@@ -103,7 +103,7 @@ Tale『うたえ』参照。`
   birthday: "1月17日",
   blood: "A型",
   height: "155cm",
-  work: "勿忘草",
+  work: `<a href="https://minecraft-mcworld.com/94746/">Re:勿忘草</a>`,
   mark: "🚪",
   description:
 `Re:勿忘草の主人公。
@@ -120,7 +120,7 @@ Tale『うたえ』参照。`
   birthday: "3月7日",
   blood: "A型",
   height: "156cm",
-  work: "勿忘草／プロローグ",
+  work: `<a href="https://minecraft-mcworld.com/94746/">Re:勿忘草</a>/`+`<a href="https://fantia.jp/posts/3013778">プロローグ</a>`,
   mark: "🫧",
   description:
 `メイの親友。
@@ -134,20 +134,20 @@ Tale『うたえ』参照。`
 },
 "8th": {
   name: "フローギアス",
-  age: "9（アモル）→10〜113（夜ノ御拾イ）→114（星喰海歌）",
+  age: "9（アモルの御影）→10〜113（夜ノ御拾イ）→114（星喰海歌）",
   birthday: "7月26日",
   blood: "B型",
   height: "132cm",
-  work: "アモル／夜ノ御拾イ／星喰海歌",
+  work: `<a href="https://minecraft-mcworld.com/49367/">アモルの御影</a>/` + `<a href="https://minecraft-mcworld.com/14679/">夜ノ御拾イ</a>/`+ `<a href="https://minecraft-mcworld.com/40633/">星喰海歌</a>`,
   mark: "🌟",
   description:
 `「ねえ、きみ、今幸せ？」
 
-憎悪と召喚の名のもとに、幸せそうな人を片っ端から夜の街に攫っては殺している少女。界隈での愛称は「フロギ」。アモルの時はただ「愛」が知りたいというのが行動原理だったが、
+憎悪と召喚の名のもとに、幸せそうな人を片っ端から夜の街に攫っては殺している少女。界隈での愛称は「フロギ」。アモルの御影の時はただ「愛」が知りたいというのが行動原理だったが、
 Tale『一番星』を経て、幸せなお人間ぶち殺しマシーンと化した。本編後のフロギはガムとイトのことが大好き。
 
 ▶︎権能『召喚』
-物体／事象を召喚できる能力。
+物体/事象を召喚できる能力。
 机の上にりんごを召喚したいと思えばりんごが出てくるし、暑いと思えばそこにシベリアの空気を召喚することもできる。物体を召喚する場所に物があった場合、その物は消し飛び、上書きのような形で召喚される。あくまで「対象を召喚」する能力のため、
 全宇宙に存在しない物は召喚できない。「生み出す」のではなく、「持ってくる」イメージ。`
 },
@@ -157,11 +157,11 @@ Tale『一番星』を経て、幸せなお人間ぶち殺しマシーンと化�
   birthday: "10月3日",
   blood: "O型",
   height: "165cm",
-  work: "アモル",
+  work: `<a href="https://minecraft-mcworld.com/49367/">アモルの御影</a>`,
   mark: "👊",
   description:
 `イトの幼なじみ。
-アモル組のパンチの方。
+アモルの御影組のパンチの方。
 
 本編ではパンチ力増強装置を渡されているが、そうでなくとも素で本気を出せば木製のドアをぶち破れるくらいには強い。
 将来の夢はsp。
@@ -174,11 +174,11 @@ Tale『一番星』を経て、幸せなお人間ぶち殺しマシーンと化�
   birthday: "3月30日",
   blood: "A型",
   height: "167cm",
-  work: "アモル",
+  work: `<a href="https://minecraft-mcworld.com/49367/">アモルの御影</a>`,
   mark: "🌪",
   description:
 `ガムの幼なじみ。
-アモル組の風で飛ぶ方。
+アモルの御影組の風で飛ぶ方。
 Taleではフローギアスのことを「フロちゃん」と呼んでいる。女子力がとても高く、子供好きで家事も料理も得意。
 
 「ガムの事どう思ってるの？」と聞けば、
@@ -191,7 +191,7 @@ Taleではフローギアスのことを「フロちゃん」と呼んでいる�
   birthday: "6月15日",
   blood: "O型",
   height: "164cm",
-  work: "星喰海歌",
+  work: `<a href="https://minecraft-mcworld.com/40633/">星喰海歌</a>`,
   mark: "⛩🌸",
   description:
 `関西弁の巫女。
@@ -210,7 +210,7 @@ Taleではフローギアスのことを「フロちゃん」と呼んでいる�
   birthday: "8月21日",
   blood: "AB型",
   height: "158cm",
-  work: "MythTake",
+  work: `<a href="https://minecraft-mcworld.com/118986/">MythTake</a>`,
   mark: "🎀🦋",
   description:
 `地雷系ファッション少女。
@@ -229,7 +229,7 @@ Taleではフローギアスのことを「フロちゃん」と呼んでいる�
   birthday: "8月12日",
   blood: "A型",
   height: "161cm",
-  work: "MythTake",
+  work: `<a href="https://minecraft-mcworld.com/118986/">MythTake</a>`,
   mark: "👓",
   description:
 `穏やかな少年。
@@ -245,7 +245,7 @@ Taleではフローギアスのことを「フロちゃん」と呼んでいる�
   birthday: "1月1日",
   blood: "A型",
   height: "174cm",
-  work: "水鏡物語",
+  work: `<a href="https://minecraft-mcworld.com/65654/">水鏡物語</a>`,
   mark: "🪞",
   description:
 `神様。白フードに並ぶ、この世界の最高権力者。
@@ -262,7 +262,7 @@ Taleではフローギアスのことを「フロちゃん」と呼んでいる�
   birthday: "4月6日",
   blood: "B型",
   height: "140cm",
-  work: "水鏡物語",
+  work: `<a href="https://minecraft-mcworld.com/65654/">水鏡物語</a>`,
   mark: "🎀🐈‍⬛",
   description:
 `とても明るい小学生。
@@ -278,7 +278,7 @@ Taleではフローギアスのことを「フロちゃん」と呼んでいる�
   birthday: "11月12日",
   blood: "O型",
   height: "174cm",
-  work: "スカーケル",
+  work: `<a href="https://minecraft-mcworld.com/61008/">スカーケル</a>`,
   mark: "🛑🧵",
   description:
 `名前パンデミック男。
@@ -297,7 +297,7 @@ Taleではフローギアスのことを「フロちゃん」と呼んでいる�
   birthday: "6月3日",
   blood: "AB型",
   height: "150cm",
-  work: "スカーケル",
+  work: `<a href="https://minecraft-mcworld.com/61008/">スカーケル</a>`,
   mark: "💙",
   description:
 `『スカーケル』の主人公。
@@ -317,7 +317,7 @@ Tale『I'm fine』で
   birthday: "2月6日",
   blood: "O型",
   height: "154cm",
-  work: "若幸のソラ",
+  work: `<a href="https://minecraft-mcworld.com/76120/">若幸のソラ</a>`,
   mark: "☘",
   description:
 `『若幸のソラ』の主人公。
@@ -335,7 +335,7 @@ Tale『I'm fine』で
   birthday: "4月5日",
   blood: "A型",
   height: "167cm",
-  work: "若幸のソラ",
+  work: `<a href="https://minecraft-mcworld.com/76120/">若幸のソラ</a>`,
   mark: "💧",
   description:
 `若葉の友達。
@@ -354,7 +354,7 @@ Tale『I'm fine』で
   birthday: "7月14日",
   blood: "AB型",
   height: "148cm",
-  work: "若幸のソラ",
+  work: `<a href="https://minecraft-mcworld.com/76120/">若幸のソラ</a>`,
   mark: "🧊",
   description:
 `若葉の友達。
@@ -370,7 +370,7 @@ Tale『I'm fine』で
   birthday: "9月2日",
   blood: "A型",
   height: "156cm",
-  work: "若幸のソラ",
+  work: `<a href="https://minecraft-mcworld.com/76120/">若幸のソラ</a>`,
   mark: "✋",
   description:
 `若葉の友達。
@@ -379,6 +379,237 @@ Tale『I'm fine』で
 若葉の呪いを肩代わりするため、神様への捧げ物となった。
 
 本編では『命令』を出してくるボスとして登場。`
+},
+"22th": {
+  name: "魅楽理 凛",
+  age: "15",
+  birthday: "12月22日",
+  blood: "O型",
+  height: "160cm",
+  work: `<a href="https://minecraft-mcworld.com/31858/">凛命霞</a>`,
+  mark: "✨",
+  description: `快活イケイケ女の子。ド陽キャ。一軍。友達とスタバの新作の話で盛り上がってそう。でも友達想いのとてもいい子。
+
+本編では4人の子供(ワタ、シノ、セイ、シン)を殺し回るというとんでもない苦痛の旅を強いられている。可哀想。
+
+霞の『大親友』。`
+},
+"23th": {
+  name: "柊 霞",
+  age: "15",
+  birthday: "1月9日",
+  blood: "B型",
+  height: "150cm",
+  work: `<a href="https://minecraft-mcworld.com/31858/">凛命霞</a>`,
+  mark: "☁",
+  description: `物静かな少女。凛のクラスメイトであり、凛に対して激重感情を抱いている。
+
+本編では自身の爪を剥いでまで凛の精神を自身の中に受け入れ、会いに行っている。
+
+Taleではシリアスだったり暴れてたり、色々している。`
+},
+"24th": {
+  name: "アルビノ",
+  age: "9",
+  birthday: "9月28日",
+  blood: "B型",
+  height: "128cm",
+  work: `<a href="https://minecraft-mcworld.com/21697/">ConneCtia</a>/` + `<a href="https://minecraft-mcworld.com/106221/">Afterlife Bitterland</a>`,
+  mark: "🤍💥",
+  description: `大爆発(物理)ロリ。ジオの妹。
+性格は物静かだが、それに見合わない爆発とかいうハジケリストみたいな権能を授けられた。過去に人体実験に巻き込まれたことがあり、声帯が無く喋ることができない。信じた相手にひたすら尽くす性格をしている。
+
+▶︎権能『爆発』:自身の命を削って爆発を生み出す能力。
+爆発の大きさ/代償は保持者の生命力依存。削れた命は、長い期間をかけて睡眠/食事をすることで少しずつ回復する。命はゲームのHPバーのような感じで、爆発を使用する度にそれが削れていくイメージ。`
+},
+"25th": {
+  name: "ジオ",
+  age: "15",
+  birthday: "5月8日",
+  blood: "O型",
+  height: "170cm",
+  work: `<a href="https://minecraft-mcworld.com/21697/">ConneCtia</a>`,
+  mark: "🟥",
+  description: `正義感の強いアルビノの兄。
+人体実験に巻き込まれたアルビノを救い出した事がある。
+爽やかイケメン。
+
+フロギが嫉妬する量の友達がいるのと、実はめちゃくちゃ量食べる。が、代謝が凄く良いのでなんとかなっている。羨ましい。`
+},
+"26th": {
+  name: "カルミア",
+  age: "200〜",
+  birthday: "12月3日",
+  blood: "A型",
+  height: "175cm",
+  work: `<a href="https://minecraft-mcworld.com/21697/">ConneCtia</a>`,
+  mark: "🟪🪡",
+  description: `別名『紫』。自身の持つ権能で自分だけのコロニーをいそいそ作っていた。が、本編ではジオとかアルビノとか色んな人が自分のパーソナルスペースに転がりこんできた可哀想な人。
+コロニー内では何でもできるが、外では非力なので大の男1人に普通に捕まる。
+得意なことは裁縫と嘘。
+
+▶︎権能『接続』:物体/時間軸を接続できる能力。
+別の時間軸から物や人を引っ張りあげてきたり、2つの事象を接続して合成することもできる。(例:重水素と三重水素を接続して水素爆弾を創り出す、など)ConneCtiaでは、カルミアが別の時間軸から土地やら建築やらをコロニー内に接続して、コロニーの土地を増やしていた。`
+},
+"27th": {
+  name: "陽樹/伊月",
+  age: "0",
+  birthday: "4月27日",
+  blood: "A/AB型",
+  height: "160cm/155cm",
+  work: `<a href="https://minecraft-mcworld.com/30221/">GeminiSoul</a>`,
+  mark: "☀️/🌙",
+  description: `GeminiSoulの双子。
+
+世界の理によって、同じ星の元に生まれられない。
+でも、僕らがもし離れ離れになったとしても、絶対、君のことを忘れないからね。
+
+......ほんとだよ。`
+},
+"28th": {
+  name: "赤原 笑美",
+  age: "16",
+  birthday: "3月15日",
+  blood: "O型",
+  height: "160cm",
+  work: `<a href="https://minecraft-mcworld.com/19118/">忘却のエミ</a>`,
+  mark: "❄️🧣",
+  description: `雪山にいた笑わない少女。
+笑蘭の姉で一人称は｢ボク｣。
+「...君、なにしてるんだい...？」といった口調で喋る。引いてる顔が似合う子。
+風邪を引いた時は焚き火で縮こまっている。`
+},
+"29th": {
+  name: "赤原 笑蘭",
+  age: "14",
+  birthday: "3月15日",
+  blood: "A型",
+  height: "148cm",
+  work: `<a href="https://minecraft-mcworld.com/56464/">NaPice</a>`,
+  mark: "🔪🩸",
+  description: `この人間には日本国憲法は適用されないので、いくらでも殴ることが可能。
+笑美の妹であり、鉱山の「NPC」として存在している。
+この子だけ妙にDVや流血FAが多いが、日本国憲法が適用されていないので何も問題はない。`
+},
+"30th": {
+  name: "露田 琥珀",
+  age: "15（ラストピース）→17（星喰海歌）",
+  birthday: "8月1日",
+  blood: "A型",
+  height: "182cm",
+  work: `<a href="https://minecraft-mcworld.com/3033/">ラストピース</a> /` + `<a href="https://minecraft-mcworld.com/40633/">星喰海歌</a>`,
+  mark: "⛏",
+  description: `終末のラストピースや星喰海歌などの主人公。
+冷静な少年。身近な人でも敬語で話すタイプ。
+真面目そうだけど割と毒舌なところある。かわいいね。
+ラストピースの世界で寧夢と出会い、星喰海歌の世界で海露と出会った。
+公式CPは寧夢だけど別に付き合ってない。`
+},
+"31th": {
+  name: "楠野 寧夢",
+  age: "6（命の檻）→9（アマノガワ）→10（ibの散り際に）→14（ラストピース）",
+  birthday: "9月17日",
+  blood: "A型",
+  height: "168cm",
+  work: "命の檻/アマノガワ/ibの散り際に/ラストピース",
+  mark: "🌸",
+  description: `ラストピースが初出だと思われがちだが、ibの散り際にが初出。あとアマノガワの主人公。快活少女であり、mshキャラ屈指のまとも枠。琥珀の事が普通に好き。かわいいね。
+ちなみにエンドロールは彼らの子供の物語である。
+
+▶︎権能『目星』を所持している。見えないものが視える能力。
+第四の壁を超えてプレイヤーや上の世界を知覚したり、他人の過去/未来を視ることができる。
+保持している寧夢は上位存在ではない人間カウントのため、一般人にバレたら白フードに轢き殺される。`
+},
+"32th": {
+  name: "ダイナ",
+  age: "17",
+  birthday: "10月14日",
+  blood: "C型",
+  height: "165cm",
+  work: `<a href="https://minecraft-mcworld.com/165871/">মৃত্যুদণ্ড罰罰病</a>`,
+  mark: "🦇🍷",
+  description: `吸血鬼の少女。
+種族が違うので人間とは血液の型が異なる。
+
+本編では吸血鬼にとっての食事のため、歩いていた人間の血を吸ってしまった。本人はとても後悔している。
+人間の首筋を噛むことで、その人間と運命共同体になる。`
+},
+"33th": {
+  name: "レラララ",
+  age: "10",
+  birthday: "11月2日",
+  blood: "B型",
+  height: "149cm",
+  work: `<a href="https://minecraft-mcworld.com/165871/">মৃত্যুদণ্ড罰罰病</a>`,
+  mark: "💛🪄",
+  description: `通称『レラ』。
+見たものを破壊したり、浮かせたり、違うものを見せたりできる。
+権能で街をまるごと壊したことがあり、死刑判決後にホスピスに連れて来られた。
+寧夢と同じく『目星』の権能を所持しているが、最大限引き出せている。`
+},
+"34th": {
+  name: "オーダー",
+  age: "32",
+  birthday: "12月12日",
+  blood: "A型",
+  height: "178cm",
+  work: `<a href="https://minecraft-mcworld.com/165871/">মৃত্যুদণ্ড罰罰病</a>`,
+  mark: "🏥🟩",
+  description: `医者の男性。
+なんでも治す能力を持っているが、治療対象に苦痛を伴うことがある。
+
+苦痛に耐えきれず｢殺してくれ｣と志願してきた患者を殺し続け、精神を自身の能力で無理やり落ち着かせながら過ごしている。`
+},
+"35th": {
+  name: "イディオット",
+  age: "12",
+  birthday: "4月17日",
+  blood: "B型",
+  height: "155cm",
+  work: `<a href="https://minecraft-mcworld.com/165871/">মৃত্যুদণ্ড罰罰病</a>`,
+  mark: "🟦🌎",
+  description: `レンの弟であり、アザトースの器。
+
+ホスピスを作った張本人であり、自身の兄と出会うためにホスピスを作った。
+
+彼は/-4%78^[閲覧する権限がありません]`
+},
+"36th": {
+  name: "主人公(LostMercy)",
+  age: "14",
+  birthday: "9月8日",
+  blood: "অ্যাজাথোজ型",
+  height: "159cm",
+  work: `<a href="https://minecraft-mcworld.com/16598/">LostMercy</a>`,
+  mark: "🐈‍⬛🦇",
+  description: `LostMercyの主人公。
+白い箱の実験に巻き込まれ、様々な自分の前世を取り込んでいる。
+
+前世のエネルギーを取り込む際、人格の一部が動物寄りになり、猫を取り込んだ時は「にゃーん」と鳴くこともある。`
+},
+"37th": {
+  name: "リバ",
+  age: "20",
+  birthday: "10月20日",
+  blood: "O型",
+  height: "185cm",
+  work: `<a href="https://minecraft-mcworld.com/205746/">めるとぴあ</a>`,
+  mark: "🦈",
+  description: `関西弁の金髪ハーフアップ。
+怖く見られがちだが根はとても優しい。
+最近お酒が飲めるようになったらしい。`
+},
+"38th": {
+  name: "メルピナ",
+  age: "9+3=12",
+  birthday: "5月5日",
+  blood: "-型",
+  height: "156cm",
+  work: `<a href="https://minecraft-mcworld.com/205746/">めるとぴあ</a>`,
+  mark: "💠",
+  description: `右目に青い花の眼帯を付けている少女。
+好奇心が強く、リバにとても懐いている。
+普段は右利きだが、使おうとすれば両方使えるらしい。すごい。`
 },
 };
 
